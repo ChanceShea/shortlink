c54e0dc5e65b3b5be8574b50ed77208518cbedd1
Shea <2788456221@qq.com>
2025-05-12 15:24:20 +0800

    MyBatisPlus分页插件
    Signed-off-by: Shea <2788456221@qq.com>

---
+++ project/src/main/java/com/shea/project/common/config/DataBaseConfiguration.java
@@ -0,0 +1,26 @@
+package com.shea.project.common.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: TODO
+ * @Author: Shea.
+ * @Date: 2025/5/12 14:34
+ */
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * MybatisPlus分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
