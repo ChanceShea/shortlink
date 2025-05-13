f1e891c54678f8001adfe2dfd4a9b90576b761c7
Shea <2788456221@qq.com>
2025-05-13 18:21:28 +0800

    开发短链接创建时获取原始网站图标
    Signed-off-by: Shea <2788456221@qq.com>

--- project/src/main/java/com/shea/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/shea/project/service/impl/ShortLinkServiceImpl.java
@@ -25,7 +25,11 @@ import com.shea.project.toolkit.ShortLinkUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.redisson.api.RBloomFilter;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
@@ -35,6 +39,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -72,6 +78,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<IShortLinkMapper, ShortLin
         shortLinkDO.setFullShortUrl(fullShortUrl);
         shortLinkDO.setShortUri(suffix);
         shortLinkDO.setEnableStatus(0);
+        shortLinkDO.setFavicon(getFavicon(shortLinkCreateReqDTO.getOriginUrl()));
 
         ShortLinkGotoDO shortLinkGotoDO = ShortLinkGotoDO.builder()
                 .gid(shortLinkCreateReqDTO.getGid())
@@ -249,5 +256,22 @@ public class ShortLinkServiceImpl extends ServiceImpl<IShortLinkMapper, ShortLin
             customGenerateCount++;
         }
         return shortUri;
+    }
+
+    @SneakyThrows
+    private String getFavicon(String url) {
+        URL targetUrl = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) targetUrl.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        int responseCode = connection.getResponseCode();
+        if (HttpURLConnection.HTTP_OK == responseCode) {
+            Document document = Jsoup.connect(url).get();
+            Element faviconLink = document.select("link[rel~=(?i)^(shortcut )?icon]").first();
+            if (faviconLink != null) {
+                return faviconLink.attr("abs:href");
+            }
+        }
+        return null;
     }
 }
